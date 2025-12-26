65cb6b24632fc5c6bcac0c40015bf793ab447eb8
unknown <aayushsuryawanshi4490@gmail.com>
2025-12-26 14:04:14 +0530

    practical pfc

---
+++ practical7/swapnumbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    swap(&x, &y);
+
+    printf("After swap: x = %d y = %d", x, y);
+    return 0;
+}
+//OUTPUT
+// Enter two numbers: 5
+// 7
+// After swap: x = 7 y = 5
